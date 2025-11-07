7545b867de96a42ec2376407caba1093954694b2
Justin Caveda <justincaveda11031@gmail.com>
2025-11-06 20:35:18 -0600

    Refactor Neovim config to use mini.deps (#38)

--- files/system/usr/share/bluebuild/dotfiles/.config/nvim/init.lua
+++ files/system/usr/share/bluebuild/dotfiles/.config/nvim/init.lua
@@ -1,160 +1,100 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if vim.loader then
+  vim.loader.enable()
+end
+
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Core options
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.wrap = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+
+-- Bootstrap mini.nvim for dependency management via mini.deps
+local mini_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
   vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
+    "git",
+    "clone",
+    "https://github.com/echasnovski/mini.nvim",
+    mini_path,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(mini_path)
 
--- Basic settings
-vim.g.mapleader = " "
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.termguicolors = true
+local MiniDeps = require("mini.deps")
+MiniDeps.setup({ path = { package = vim.fn.stdpath("data") .. "/site/pack/deps" } })
 
--- Plugins
-local plugins = {
-  -- Color scheme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight-night")
-    end,
-  },
+local add = MiniDeps.add
+add("echasnovski/mini.nvim")
+add("lewis6991/gitsigns.nvim")
+add("neovim/nvim-lspconfig")
 
-  -- Treesitter for syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "bash", "yaml", "json", "markdown" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+-- Mini modules
+require("mini.pairs").setup()
+require("mini.comment").setup()
+require("mini.statusline").setup()
+require("mini.files").setup()
+require("mini.pick").setup()
 
-  -- File tree
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup()
-    end,
-  },
+-- Git signs
+require("gitsigns").setup()
 
-  -- Fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+-- LSP
+local lspconfig = require("lspconfig")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-  -- LSP and autocomplete
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
+local function on_attach(client, bufnr)
+  local map = function(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+  end
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-      })
+  map("n", "gd", vim.lsp.buf.definition, "LSP: Go to definition")
+  map("n", "gr", vim.lsp.buf.references, "LSP: Go to references")
+  map("n", "K", vim.lsp.buf.hover, "LSP: Hover")
+  map({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, "LSP: Format")
+  map("n", "[d", vim.diagnostic.goto_prev, "LSP: Previous diagnostic")
+  map("n", "]d", vim.diagnostic.goto_next, "LSP: Next diagnostic")
+  map("n", "<leader>cd", vim.diagnostic.open_float, "LSP: Line diagnostics")
+end
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig.util")
-
-      local server_overrides = {
-        pyright = {
-          root_dir = util.root_pattern("pyproject.toml", "setup.py", "requirements.txt", ".git"),
-        },
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = { globals = { "vim" } },
-            },
-          },
-        },
-      }
-
-      local servers = { "bashls", "pyright", "yamlls", "lua_ls" }
-      for _, server in ipairs(servers) do
-        local opts = vim.tbl_deep_extend(
-          "force",
-          { capabilities = capabilities },
-          server_overrides[server] or {}
-        )
-        lspconfig[server].setup(opts)
-      end
-    end,
-  },
-
-  -- Git signs
-  {
-    "lewis6991/gitsigns.nvim",
-    config = true,
-  },
-
-  -- Mini modules (pairs, comment, statusline)
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require("mini.pairs").setup()
-      require("mini.comment").setup()
-      require("mini.statusline").setup()
-    end,
-  },
-}
-
-require("lazy").setup(plugins)
+local servers = { "bashls", "pyright", "yamlls", "lua_ls" }
+for _, server in ipairs(servers) do
+  local opts = { capabilities = capabilities, on_attach = on_attach }
+  if server == "lua_ls" then
+    opts.settings = {
+      Lua = {
+        diagnostics = { globals = { "vim" } },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      },
+    }
+  end
+  lspconfig[server].setup(opts)
+end
 
 -- Keymaps
 local function map(mode, lhs, rhs, desc)
   vim.keymap.set(mode, lhs, rhs, { desc = desc })
 end
 
-local function nmap(lhs, rhs, desc)
-  map("n", lhs, rhs, desc)
-end
+local pick = require("mini.pick")
+map("n", "<leader>ff", pick.builtin.files, "Find files")
+map("n", "<leader>fg", pick.builtin.grep_live, "Live grep")
+map("n", "<leader>fb", pick.builtin.buffers, "List buffers")
+map("n", "<leader>e", require("mini.files").open, "Toggle file explorer")
+map("n", "<leader>qq", vim.cmd.quit, "Quit")
+map("n", "<leader>ww", vim.cmd.write, "Write file")
 
-nmap("<leader>ff", "<cmd>Telescope find_files<cr>", "Find files")
-nmap("<leader>fg", "<cmd>Telescope live_grep<cr>", "Live grep")
-nmap("<leader>fb", "<cmd>Telescope buffers<cr>", "List buffers")
-nmap("<leader>e", "<cmd>NvimTreeToggle<cr>", "Toggle file tree")
